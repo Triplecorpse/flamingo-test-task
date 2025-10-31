11ee1d7c5f7abe5ca32d5bdc8c823090d52eb437
Eldar Khaitov <eldar.khaitov@gmail.com>
2025-10-31 14:00:41 +0100

    fixed google oauth

--- app/services/oauth.server.ts
+++ app/services/oauth.server.ts
@@ -1,21 +1,26 @@
 import {OAuth2Client} from 'google-auth-library';
 
 const client = new OAuth2Client(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_OAUTH_CLIENT_ID,
+    process.env.GOOGLE_OAUTH_CLIENT_SECRET,
     process.env.GOOGLE_REDIRECT_URI
 );
 
 export async function verifyGoogleToken(token: string) {
     const ticket = await client.verifyIdToken({
         idToken: token,
-        audience: process.env.GOOGLE_CLIENT_ID
+        audience: process.env.GOOGLE_OAUTH_CLIENT_ID
     });
     return ticket.getPayload();
 }
 
 export async function getGoogleIdToken(code: string) {
-    const {tokens} = await client.getToken(code);
+    try {
+        const {tokens} = await client.getToken(code);
 
-    return tokens.id_token;
+        return tokens.id_token;
+    } catch (error) {
+        console.error('Error getting Google ID token:', error);
+        throw error;
+    }
 }
