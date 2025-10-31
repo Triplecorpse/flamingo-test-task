148d03ce9d6e47e2b016c27ecf4537f5437cd48a
Eldar Khaitov <eldar.khaitov@gmail.com>
2025-10-31 16:06:49 +0100

    An attempt to fix Vercel #4

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/**** Tailwind CSS config in CommonJS for Vercel ****/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
